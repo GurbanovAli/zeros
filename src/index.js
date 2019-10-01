60f3d9afedf47f6c8efa72500ecdc4721bf8bb0b
GurbanovAli <50907064+GurbanovAli@users.noreply.github.com>
2019-10-01 16:59:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let arr1 = []
+  let arr2 = []
+  let number = 0
+  let number2 = 0
+
+  expression.split('*').map(v => v[v.length - 2] === '!' ? arr2.push(v.substr(0, v.length - 2)) : arr1.push(v.substr(0, v.length - 1)));
+
+  function countNumberAndNumber2(v, version) {
+    for (v; v > 0; v -= version) {
+      let num = v;
+      while (true) {
+        if (num % 2 === 0) {
+          number += 1;
+          num /= 2;
+        } else if (num % 5 === 0) {
+          number2 += 1;
+          num /= 5;
+        } else break;
+      }
+    }
+  }
+
+  arr1.map(v => countNumberAndNumber2(v, 1));
+  arr2.map(v => countNumberAndNumber2(v, 2));
+  return number - number2 > 0 ? number2 : number;
 }
